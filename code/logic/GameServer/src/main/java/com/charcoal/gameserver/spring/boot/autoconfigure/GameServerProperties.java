be446694879b4632e762493817ed6f048240274b
xiao tan <254180937@qq.com>
2018-11-10 22:43:15 +0800

    commit README.md

---
+++ code/logic/GameServer/src/main/java/com/charcoal/gameserver/spring/boot/autoconfigure/GameServerProperties.java
@@ -0,0 +1,40 @@
+package com.charcoal.gameserver.spring.boot.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @Auther: wumingjie
+ * @Date: 2018/11/10
+ * @Description:
+ */
+@ConfigurationProperties(prefix = GameServerProperties.GAME_SERVER_PREFIX)
+public class GameServerProperties {
+    public static final String GAME_SERVER_PREFIX = "gameServer";
+    private String ip;
+    private String port;
+    private int workPoolSize;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public int getWorkPoolSize() {
+        return workPoolSize;
+    }
+
+    public void setWorkPoolSize(int workPoolSize) {
+        this.workPoolSize = workPoolSize;
+    }
+}
